ec4266772eebd0c39fe85b3e17146ba21d6be3af
spustoff <vyacheslavpustov05@gmail.com>
2023-11-20 15:51:09 +0300

    Initial Commit

---
+++ App250/App250App.swift
@@ -0,0 +1,17 @@
+//
+//  App250App.swift
+//  App250
+//
+//  Created by Вячеслав on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct App250App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
